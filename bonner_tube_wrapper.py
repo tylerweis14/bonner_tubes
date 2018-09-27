79f65f9dc9ded4a794049399cf72f0225dacc0b6
johnboyington <mjcb@ksu.edu>
2018-09-27 12:00:16 -0500

    set up wrapper structure

--- bonner_tube_wrapper.py
+++ bonner_tube_wrapper.py
@@ -1,9 +1,49 @@
 from bonner_tube_template import template
 import numpy as np
 from multigroup_utilities import energy_groups
-s = template
-eb = energy_groups()[::-1]*1e-6
-n = len(eb)
-for i in range(n-1):
-    s = s.format(eb[i],eb[i+1])
-    print(s)
+import os
+
+
+
+
+
+
+def write_input(erg_bounds, mat, template):
+    """Writes an mcnp input file."""
+    mats = {}
+    mats['abs'] = (3, 1.070)
+    mats['poly'] = (4, 1.300)
+    template = template.format(*mats[mat], *mats[mat], *erg_bounds)
+    with open('input.i', 'w+') as F:
+        F.write(template)
+    return
+
+
+def run_input():
+    """Runs the mncp input file."""
+    print('run_input not implemented yet.')
+    return
+
+def extract_output():
+    """Grabs the output value from the mcnp output file."""
+    print('extract_output not implemented yet.')
+    return
+
+def clean_repo():
+    """Removes the mcnp files from the repository."""
+    for f in ['input.i', 'input.io', 'input.ir']:
+        try:
+            os.system('rm {}'.format(f))
+        except:
+            pass
+    return
+
+
+
+if __name__ == '__main__':
+    eb = energy_groups()[::-1]*1e-6
+    for i in range(len(eb)-1):
+        write_input((eb[i],eb[i+1]), 'abs', template)
+        run_input()
+        extract_output()
+        clean_repo()
