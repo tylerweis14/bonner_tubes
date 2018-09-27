280adcb76d9f8bc705df99bcd0af65e7b9b9ccd9
John Boyington <mjcb@ksu.edu>
2018-09-27 15:53:05 -0500

    may work now

--- master_slave.py
+++ master_slave.py
@@ -30,28 +30,35 @@ def master_task():
                 jobs.append((name+str(i), (eb[i], eb[i+1]), m, l, template))
 
     # distribute work
-    while open_jobs:
+    slave_exit_status = [False] * size
+    slave_exit_status[0] = True
+    while True:
         # find slave and establish communication
         ready_node = comm.recv(MPI.ANY_SOURCE, tag='REQUEST')
         comm.send(ready_node, tag='INQUIRE')
         slave_status = comm.recv(ready_node, tag='STATUS')
 
-        if slave_status == 'IDLE':
-            next_job = jobs.pop()
-            comm.send(next_job, ready_node, tag='JOB')
-            open_jobs.append(next_job)
-        elif slave_status == 'COMPLETE':
-            job, result = comm.recv(ready_node, tag='DATA')
-            name = '{}_{}cm'.format(job[0], job[1])
-            rf[name][0] = result[0]
-            rf[name][1] = result[1]
-            closed.append(job)
-            open_jobs.remove(job)
-            # store_data()
+        if jobs or open_jobs:
+            if slave_status == 'IDLE':
+                next_job = jobs.pop()
+                comm.send(next_job, ready_node, tag='JOB')
+                open_jobs.append(next_job)
+            elif slave_status == 'COMPLETE':
+                job, result = comm.recv(ready_node, tag='DATA')
+                name = '{}_{}cm'.format(job[0], job[1])
+                rf[name][0] = result[0]
+                rf[name][1] = result[1]
+                closed.append(job)
+                open_jobs.remove(job)
 
-    # tell slaves to quit
-    for i in range(1, size):
-        comm.send('QUIT', i, tag=i)
+        # tell slaves to quit
+        else:
+            comm.send('QUIT', i, tag=i)
+            slave_exit_status[i] = True
+            if False in slave_exit_status:
+                pass
+            else:
+                break
     return
 
 
@@ -68,16 +75,12 @@ def slave_task():
         # recieve job from master
         if status == 'IDLE':
             job = comm.recv(source=0, tag='JOB')
+            if job == 'QUIT':
+                break
             status = 'BUSY'
             data = run_mcnp(*job)
             status = 'COMPLETE'
         elif status == 'COMPLETE':
             comm.send(data, dest=0, tag='DATA')
             status = 'IDLE'
-        
-        if task == 'QUIT':
-            break
-        else:
-            #run_mcnp()
-            pass
     return
