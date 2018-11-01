3ddf1adfed22cf1b22b10b60778ab838d4826412
John Boyington <mjcb@ksu.edu>
2018-11-01 09:47:25 -0500

    added backplug and bp; ran cases

--- plot_rfs.py
+++ plot_rfs.py
@@ -1,5 +1,4 @@
 import pickle
-import numpy as np
 import matplotlib.pyplot as plt
 from parameters import params
 from spectrum import Spectrum
@@ -10,17 +9,14 @@ with open('btube_rf_data.p', 'rb') as F:
     rf = pickle.load(F)
 
 
-for key, val in rf.items():
-    print(key)
-
-fig = plt.figure(1)
+# setup plotting environment
+fig = plt.figure(0)
 ax = fig.add_subplot(111)
 ax.set_xscale('log')
 ax.set_yscale('log')
 ax.set_ylim(1e-6, 3e-2)
 lengths = []
-length = [0.001,0.25,0.5,0.75,1,1.5,2.0,3.0,4.0,6]
-#length = [0.001,0.25,0.5,0.75,1]
+length = [0.001, 0.25, 0.5, 0.75, 1, 1.5, 2.0, 3.0, 4.0, 6]
 for i in length:
     lengths.append(i*2.54)
 for key, val in rf.items():
@@ -29,7 +25,6 @@ for key, val in rf.items():
         r, err = val[1:].T
         xs = Spectrum(params['eb'], r, r*err)
         plot = ax.plot(*xs.stepu, label=key)
-        print(xs.error)
         ax.errorbar(xs.midpoints, xs.values, xs.values * err, linestyle='None', color=plot[0].get_color())
 
 ax.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
